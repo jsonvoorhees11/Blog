635c08f9a3642a60cd15e2d53f3ebc6ae8004956
jsonvoorhees <little.shadowfiend@gmail.com>
2019-06-20 19:19:11 +0700

    Add unit tests for service and mapper

---
+++ Blog.Test/Tests/MapperUnitTests/ArticleMappingUnitTests.cs
@@ -0,0 +1,49 @@
+using Blog.DataAccess.Entities;
+using Blog.Mappers;
+using Blog.Models;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Blog.Test{
+    public class ArticleMappingUnitTests{
+        [Fact]
+        public void MappingArticleToArticleDto_ShouldReturnArticleDto_WhenArticleValid()
+        {
+        //Arrange
+            var articleMapper = new ArticleMapper();
+            var articleJsonString = @"{""id"":""9a574346-a3a5-4860-a3bd-54be358ba236"",
+                        ""title"":""How .NET compiler work?"",
+                        ""slug"":""how-net-compiler-work"",
+                        ""thumbnailImageUrl"":""https://localhost:7070/api/fileUploader/netcompiler.png"",
+                        ""recap"":"".NET compiler is really complicated"",
+                        ""content"":""I don't know how it works either"",
+                        ""createdDate"": ""1560182949"",
+                        ""lastModifiedDate"":""1560182949"",
+                        ""authorId"":""f6eb594c-4c06-4dec-9412-133c2d32a549""}";
+
+            var userJsonString = @"{""id"":""87913df7-7dc9-45bb-a486-6be3a902f8c0"",
+                        ""email"":""lolgag@9gag.com"",      
+                        ""createdDate"":""1560182949"",     
+                        ""lastModifiedDate"":""1560182949""}";  
+
+            var article = JsonConvert.DeserializeObject<Article>(articleJsonString);
+            var user = JsonConvert.DeserializeObject<UserDto>(userJsonString);
+            var expected = new ArticleDto{
+                Id="9a574346-a3a5-4860-a3bd-54be358ba236",
+                Slug="how-net-compiler-work",
+                ThumbnailImageUrl = "https://localhost:7070/api/fileUploader/netcompiler.png",
+                Recap = ".NET compiler is really complicated",
+                Content="I don't know how it works either",
+                CreatedDate=1560182949,
+                LastModifiedDate=1560182949,
+                Author= user
+            }; 
+        //Act
+            var actual = articleMapper.MapEntityToDto(article);
+
+        //Assert
+            Assert.Equal(expected.Author.Id, actual.Author.Id);
+
+        }
+    }
+}
---
+++ Blog.Test/Tests/ServiceUnitTests/ArticleServiceUnitTests.cs
@@ -0,0 +1,107 @@
+using System.Collections.Generic;
+using System.Linq;
+using Blog.DataAccess;
+using Blog.DataAccess.Entities;
+using Blog.DataAccess.Seeding;
+using Blog.Mappers;
+using Blog.Models;
+using Blog.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Blog.Test{
+    public class ArticleServiceUnitTests{
+
+        [Fact]
+        public void GetAllArticles_ShouldReturnAllArticle()
+        {
+        //Arrange
+            var articles = GetArticles();
+            var dataSeedingMock = new Mock<IDataSeeding>();
+            dataSeedingMock.Setup(d=>d.GetArticles()).Returns(articles);
+            dataSeedingMock.Setup(d=>d.GetArticleCategories()).Returns(new List<ArticleCategory>());
+            dataSeedingMock.Setup(d=>d.GetArticleTags()).Returns(new List<ArticleTag>());
+            dataSeedingMock.Setup(d=>d.GetCategories()).Returns(new List<Category>());
+            dataSeedingMock.Setup(d=>d.GetComments()).Returns(new List<Comment>());
+            dataSeedingMock.Setup(d=>d.GetReaders()).Returns(new List<Reader>());
+            dataSeedingMock.Setup(d=>d.GetTags()).Returns(new List<Tag>());
+            dataSeedingMock.Setup(d=>d.GetUsers()).Returns(new List<User>());
+
+            var options = new DbContextOptionsBuilder<BlogDbContext>()
+                            .UseInMemoryDatabase(databaseName: "InMemory_Articles")
+                            .Options; 
+            IMapper<Article, ArticleDto> mapper = SetupMapperMocking();
+
+            using (var context = new BlogDbContext(dataSeedingMock.Object,options)){
+                context.Articles.AddRange(articles);
+                context.SaveChanges();
+            }
+
+            using (var context = new BlogDbContext(dataSeedingMock.Object,options)){
+                var service = new ArticleService(context,mapper);
+                var articleCount = service.GetArticles().Count();
+                Assert.True(articleCount>0);
+            }
+        }
+
+        private IEnumerable<Article> GetArticles(){
+            const string articlesJsonString = @"[
+                    {
+                        ""id"":""9a574346-a3a5-4860-a3bd-54be358ba236"",
+                        ""title"":""How .NET compiler work?"",
+                        ""slug"":""how-net-compiler-work"",
+                        ""thumbnailImageUrl"":""https://localhost:7070/api/fileUploader/netcompiler.png"",
+                        ""recap"":"".NET compiler is really complicated"",
+                        ""content"":""I don't know how it works either"",
+                        ""createdDate"": ""1560182949"",
+                        ""lastModifiedDate"":""1560182949"",
+                        ""authorId"":""f6eb594c-4c06-4dec-9412-133c2d32a549""
+                    },
+                    {
+                        ""id"":""47eef876-5eb2-442f-913c-6ad098864f9e"",
+                        ""title"":""Angular, ReactJS and VueJs, which to choose?"",
+                        ""slug"":""angular-reactjs-and-vuejs-which-to-choose"",
+                        ""thumbnailImageUrl"":""https://localhost:7070/api/fileUploader/angular-is-the-best.png"",
+                        ""recap"":""Angular definitely"",
+                        ""content"":""Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit"",
+                        ""createdDate"": ""1560182949"",
+                        ""lastModifiedDate"":""1560182949"",
+                        ""authorId"":""87913df7-7dc9-45bb-a486-6be3a902f8c0""
+                    },
+                    {
+                        ""id"":""f2e240c4-3e7e-4c87-93ce-c95ffa2941c2"",
+                        ""title"":""The rise of .NET Core"",
+                        ""slug"":""the-rise-of-net-core"",
+                        ""thumbnailImageUrl"":""https://localhost:7070/api/fileUploader/netcore.png"",
+                        ""recap"":"".NET Core is sky-rocketing"",
+                        ""content"":""At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga"",
+                        ""createdDate"": ""1560182949"",
+                        ""lastModifiedDate"":""1560182949"",
+                        ""authorId"":""f6eb594c-4c06-4dec-9412-133c2d32a549""
+                    },
+                    {
+                        ""id"":""4bcfe398-d377-43ce-a134-99f0823511d7"",
+                        ""title"":""Rust is becoming another trend"",
+                        ""slug"":""rust-is-becoming-another-trend"",
+                        ""thumbnailImageUrl"":""https://localhost:7070/api/fileUploader/rustland.png"",
+                        ""recap"":""Rust is becoming another trend"",
+                        ""content"":""Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur"",
+                        ""createdDate"": ""1560182949"",
+                        ""lastModifiedDate"":""1560182949"",
+                        ""authorId"":""f6eb594c-4c06-4dec-9412-133c2d32a549""
+                    }
+                ]";
+            
+            var articles = JsonConvert.DeserializeObject<IEnumerable<Article>>(articlesJsonString);
+            return articles;
+        }
+    
+        private IMapper<Article, ArticleDto> SetupMapperMocking(){
+            var mapperMock = new Mock<IMapper<Article, ArticleDto>>();
+            mapperMock.Setup(m=>m.MapEntityToDto(It.IsAny<Article>())).Returns(new ArticleDto());
+            return mapperMock.Object;
+        }
+    }
+}
